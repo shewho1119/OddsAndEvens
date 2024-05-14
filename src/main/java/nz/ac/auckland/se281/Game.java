3db0e1984ac5ddb34de1b01e71298c0d1a8b3158
shewho1119 <parkshihoo1119@gmail.com>
2024-05-14 17:53:56 +1200

    Ask for the number of fingers to the user

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -11,13 +11,14 @@ public class Game {
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-
     roundNumber = 0;
   }
 
   public void play() {
+    // increment the round number everytime PLAY is invoked
     roundNumber++;
     MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
+    MessageCli.ASK_INPUT.printMessage();
   }
 
   public void endGame() {}
