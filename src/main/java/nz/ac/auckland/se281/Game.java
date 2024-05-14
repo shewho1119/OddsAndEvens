602d8f1849ebc03a79fb37ed687e10df886a64c5
shewho1119 <parkshihoo1119@gmail.com>
2024-05-14 15:47:51 +1200

    When the player invoke the command PLAY, print the current number of round and increment this number everytime the PLAY is invoked.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,12 +6,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  int roundNumber;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+
+    roundNumber = 0;
   }
 
-  public void play() {}
+  public void play() {
+    roundNumber++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(roundNumber));
+  }
 
   public void endGame() {}
 
