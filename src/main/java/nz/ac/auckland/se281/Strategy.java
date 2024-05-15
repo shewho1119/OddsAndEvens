b9dd38e8d623347cd2dc7cc07223cc3e2e820920
shewho1119 <parkshihoo1119@gmail.com>
2024-05-16 10:50:48 +1200

    Created a new class that will allow us to change strategies at runtime

---
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -0,0 +1,18 @@
+package nz.ac.auckland.se281;
+
+public class Strategy {
+
+  StrategyType type;
+
+  public Strategy(StrategyType type) {
+    this.type = type;
+  }
+
+  public void setStrategy(StrategyType type) {
+    this.type = type;
+  }
+
+  public void process() {
+    type.getFingersAI();
+  }
+}
