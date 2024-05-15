cf00b739659eaf8e5de5abb31f0239d8e679ce1e
shewho1119 <parkshihoo1119@gmail.com>
2024-05-16 10:45:45 +1200

    created 2 new class and 1 new interface to build the Strategy design pattern

---
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -0,0 +1,6 @@
+package nz.ac.auckland.se281;
+
+public class RandomStrategy implements StrategyType {
+  @Override
+  public void getFingersAI() {}
+}
---
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -0,0 +1,6 @@
+package nz.ac.auckland.se281;
+
+public class TopStrategy implements StrategyType {
+  @Override
+  public void getFingersAI() {}
+}
